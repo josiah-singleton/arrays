0cf83f5def7983ff35269c0a545306355591e1c4
josiah-singleton <91489627+josiah-singleton@users.noreply.github.com>
2022-01-26 09:35:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const cars = ["Saab", "Volvo", "BMW", "mazda"];
+const spiders = ["black widow", "wolf", "trapdoor", "jumping"]
+console.log (cars)
+cars.push =("toyata")
+console.log (cars)
+console.log (spiders)
+spiders.pop(); 
+console.log (spiders)
